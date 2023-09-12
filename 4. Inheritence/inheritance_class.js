90fef3438f290cfa3737ebcc025ef9fc455c25c9
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-09-12 17:09:13 -0400

    inheritence completed

--- 4. Inheritence/inheritance_class.js
+++ 4. Inheritence/inheritance_class.js
@@ -20,11 +20,11 @@ class BankAccount {
   }
 }
 
-class CurrentAccount extends BankAccount {
+class CurrentAccount extends BankAccount { // for inherit extends is used
   transactionLimit = 50000;
 
-  constructor(customerName, balance = 0 {
-    super(customerName, balance);
+  constructor(customerName, balance = 0) {
+    super(customerName, balance); //while inheriting call super constructor which in inside BankAccount.
   }
 
   takeBusinessLoan(amount) {
@@ -35,7 +35,7 @@ class CurrentAccount extends BankAccount {
 class SavingAccount extends BankAccount {
   transactionLimit = 10000;
 
-  constructor(customerName, balance = 0 {
+  constructor(customerName, balance = 0) {
     super(customerName, balance);
   }
 
@@ -43,3 +43,20 @@ class SavingAccount extends BankAccount {
     console.log('Taking personal loan: ' + amount);
   }
 }
+
+const abhishekBankAccount = new BankAccount("Abhishek T", 3000);
+abhishekBankAccount.deposit(500);
+abhishekBankAccount.withdraw(100);
+console.log(abhishekBankAccount);
+
+const abhishekCurrentAccount = new CurrentAccount("Abhishek T", 90000);
+abhishekCurrentAccount.deposit(10000);
+abhishekCurrentAccount.withdraw(20000);
+abhishekCurrentAccount.takeBusinessLoan(50000);
+console.log(abhishekCurrentAccount);
+
+const abhishekSavingAccount = new SavingAccount("Abhishek T", 500);
+abhishekSavingAccount.deposit(10000);
+abhishekSavingAccount.withdraw(600);
+abhishekSavingAccount.takePersonalLoan(1000);
+console.log(abhishekSavingAccount);
