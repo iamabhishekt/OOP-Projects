b51c9e243417e7f51df878dfa67014eceea86914
iamabhishekt <abhishekthakur.art@protonmail.com>
2023-09-12 20:59:18 -0400

    static properties completed

---
+++ 6. Static properties and methods/Static properties and methods config eg.js
@@ -0,0 +1,7 @@
+class Config {
+  static dbUser = 'username';
+  static dbPassword = 'secret';
+  static apiToken = 'abcd';
+}
+
+console.log(Config.dbUser, Config.apiToken, Config.dbPassword);
--- 6. Static properties and methods/Static properties and methods.js
+++ 6. Static properties and methods/Static properties and methods.js
@@ -1,117 +1,56 @@
-class BankAccount {
-  customerName;
-  accountNumber;
-  #balance = 0; // _balance is old convention for private fields in JS now #balance
+// Increment
 
-  //constructor
-  constructor(customerName, balance = 0) {
-    this.customerName = customerName;
-    this.accountNumber = Date.now();
-    this.#balance = balance;
+// let id = 1; anti-pattern
+
+class User {
+  // static property
+  static id = 1;
+
+  static cache = {
+    1: "some value",
+  };
+
+  constructor(name, age, income) {
+    this.name = name;
+    this.age = age;
+    this.income = income;
+    this.id = User.id++;
+    // this.id = id++; anti-pattern
   }
 
-  //methods
-  deposit(amount) {
-    this.#balance += amount;
+  // checkCache() {
+  // }
+
+  static {
+    console.log(`Initialize`);
   }
 
-  withdraw(amount) {
-    this.#balance -= amount;
+  static hasInCache() {
+    console.log(User.cache);
   }
 
-  //public method for changing balance field (private)
-  set balance(amount) {
-    //setters(old) setBalance()
-    //validation for number only
-    if (isNaN(amount)) {
-      throw new Error("Amount is not a valid input!");
-    }
-    this.#balance = amount;
-  }
+  //static methods
+  //   static compareByAge(user1, user2) {
+  //     return user1.age - user2.age;
+  //   }
 
-  get balance() {
-    //getters(old) getBalance()
-    return this.#balance;
-  }
+  //   static compareByIncome(user1, user2) {
+  //     return user1.income - user2.income;
+  //   }
 
-  //private method
-  #calculateInterest(amount) {
-    return amount * 0.05;
-  }
-
-  displayCalculatedInterest(amount) {
-    const interest = this.#calculateInterest(amount);
-    console.log(`Calculated interest for ${amount} is ${interest}`);
-    return interest;
-  }
+  //# static properties
 }
 
-class CurrentAccount extends BankAccount {
-  // for inherit extends is used
-  transactionLimit = 50000;
+const user1 = new User("Abhishek T", 30, 3000);
+const user2 = new User("Rahul V", 26, 5000);
+const user3 = new User("Prathamesh G", 40, 10000);
 
-  constructor(customerName, balance = 0) {
-    super(customerName, balance); //while inheriting call super constructor which in inside BankAccount.
-  }
+const users = [user1, user2, user3];
 
-  takeBusinessLoan(amount) {
-    this.displayCalculatedInterest(amount);
-    console.log("Taking business loan: " + amount);
-  }
-}
+// users.sort(User.compareByAge);
+// users.sort(User.compareByIncome);
 
-class SavingAccount extends BankAccount {
-  transactionLimit = 10000;
+// console.log(users);
 
-  constructor(customerName, balance = 0) {
-    super(customerName, balance);
-  }
-
-  takePersonalLoan(amount) {
-    this.displayCalculatedInterest(amount);
-    console.log("Taking personal loan: " + amount);
-  }
-}
-
-const abhishekBankAccount = new BankAccount("Abhishek T", 3000);
-abhishekBankAccount.deposit(500);
-abhishekBankAccount.withdraw(100);
-// console.log(abhishekBankAccount);
-
-const abhishekCurrentAccount = new CurrentAccount("Abhishek T", 90000);
-
-// abhishekCurrentAccount.balance = 'Hello'; // because of encapsulation this will not happen as here it's a string and we can edit it outside of the class. it
-
-// abhishekCurrentAccount.#balance = 'Hello';
-//#balance is used to show that it is a private field and must be change within class
-
-//changing balance using public method
-// abhishekCurrentAccount.balance(400);
-// console.log(abhishekCurrentAccount.balance());
-
-//getter get balance
-console.log(abhishekCurrentAccount.balance);
-
-//setter set balance
-abhishekCurrentAccount.balance = 400;
-
-console.log(abhishekCurrentAccount.balance);
-
-abhishekCurrentAccount.takeBusinessLoan(40000);
-
-console.log(abhishekCurrentAccount);
-
-// abhishekCurrentAccount.deposit(10000);
-// abhishekCurrentAccount.withdraw(20000);
-// abhishekCurrentAccount.takeBusinessLoan(50000);
-console.log(abhishekCurrentAccount);
-
-const abhishekSavingAccount = new SavingAccount("Abhishek T", 500);
-abhishekSavingAccount.deposit(10000);
-abhishekSavingAccount.withdraw(600);
-// abhishekSavingAccount.takePersonalLoan(1000);
-// console.log(abhishekSavingAccount);
-
-abhishekSavingAccount.takePersonalLoan(30000);
-
-console.log(abhishekSavingAccount);
+User.hasInCache();
+User.hasInCache();
